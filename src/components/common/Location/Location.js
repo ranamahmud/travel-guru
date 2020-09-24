b3c9e47175a13449a346b78907c4f930a4106e6c
Md Rana Mahmud <ranamd0@gmail.com>
2020-09-24 18:37:18 +0600

    card added

---
+++ src/components/common/Location/Location.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Card, CardGroup } from 'react-bootstrap';
+
+function Location(props) {
+    return (
+        <div>
+            <CardGroup>
+                <Card>
+                    <Card.Img variant="top" src={require('../../../images/image/Rectangle 1.png')} />
+                    <Card.Body>
+                        <Card.Title>Cox's bazar</Card.Title>
+                    </Card.Body>
+
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={require('../../../images/image/Rectangle 26.png')} />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                    </Card.Body>
+
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src={require('../../../images/image/Rectangle 27.png')} />
+                    <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+
+                    </Card.Body>
+
+                </Card>
+            </CardGroup>
+        </div>
+    );
+}
+
+export default Location;
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Carousel, Container } from 'react-bootstrap';
+import { Carousel, Col, Container, Row } from 'react-bootstrap';
+import Location from '../../common/Location/Location';
 import "./Home.css"
 const Home = () => {
     return (
@@ -12,8 +13,15 @@ const Home = () => {
                         alt="First slide"
                     />
                     <Carousel.Caption>
-                        <h3>Cox's bazar</h3>
-                        <p>Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it</p>
+                        <Row>
+                            <Col><h3>Cox's bazar</h3>
+                                <p>Cox's Bazar is a city, fishing port, tourism centre and district headquarters in southeastern Bangladesh. It is famous mostly for its long natural sandy beach, and it</p>
+                                <button>Booking &#8594;</button></Col>
+                            <Col> <Location /></Col>
+                        </Row>
+
+
+
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -24,8 +32,14 @@ const Home = () => {
                     />
 
                     <Carousel.Caption>
-                        <h3>Sreemongol</h3>
-                        <p>It is said the name Sreemangal (or Srimangal) is named after Sri Das and Mangal Das; two brothers who settled on the banks of the Hail Haor.[2] A copper plate of Raja Marundanath from the 11th century was found in Kalapur</p>
+                        <Row>
+                            <Col>  <h3>Sreemongol</h3>
+                                <p>It is said the name Sreemangal (or Srimangal) is named after Sri Das and Mangal Das; two brothers who settled on the banks of the Hail Haor.[2] A copper plate of Raja Marundanath from the 11th century was found in Kalapur</p>
+                                <button>Booking &#8594;</button></Col>
+                            <Col><Location /></Col>
+                        </Row>
+
+
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -36,8 +50,15 @@ const Home = () => {
                     />
 
                     <Carousel.Caption>
-                        <h3>Sundorbon</h3>
-                        <p>The Sundarbans is a mangrove area in the delta formed by the confluence of the Ganges, Brahmaputra and Meghna Rivers in the Bay of Bengal. It spans from the Hooghly River in India's state of West Bengal to the Baleswar River in Bangladesh. Wikipedia</p>
+                        <Row>
+                            <Col><h3>Sundorbon</h3>
+                                <p>The Sundarbans is a mangrove area in the delta formed by the confluence of the Ganges, Brahmaputra and Meghna Rivers in the Bay of Bengal. It spans from the Hooghly River in India's state of West Bengal to the Baleswar River in Bangladesh. Wikipedia</p>
+                                <button>Booking <span>&#8594;</span></button></Col>
+                            <Col><Location /></Col>
+                        </Row>
+
+
+
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
