95cfd3bee2f2c10ba17b24719aa6aba9c05fca68
Md Rana Mahmud <ranamd0@gmail.com>
2020-09-24 16:29:21 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import React, { createContext } from 'react';
 import './App.css';
-import Header from './components/Header/Header';
+import Header from './components/Header/Header'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+// import NotFound from './components/NotFound/NotFound';
+
+import { useState } from 'react';
+// import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
 export const UserContext = createContext();
-
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Header />
+
+        <Switch>
+          {/* <Route path="/shop">    <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <Review></Review>
+          </Route> */}
+          {/* <PrivateRoute path="/orders">
+            <Inventory></Inventory>
+          </PrivateRoute> */}
+          {/* <Route path="/login">
+            <Login></Login>
+          </Route> */}
+          {/* <PrivateRoute path="/shipment">
+            <Shipment></Shipment>
+          </PrivateRoute>
+          <Route exact path="/">    <Shop></Shop>
+          </Route>
+          <Route path="/product/:productkey">
+            <ProductDetail></ProductDetail>
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route> */}
+        </Switch>
+
+
+      </Router>
+    </UserContext.Provider>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { useContext } from 'react';
+import { Button, Container, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../../App';
 import logo from '../../images/logo.png'
 import './Header.css'
 const Header = () => {
-    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    // const [loggedInUser, setLoggedInUser] = useContext(UserContext);
     return (
-        <div className="header">
-            <img src={logo} alt="" />
-            <nav>
-                <Link to="/shop">Shop</Link>
-                <Link to="/review">Order Review</Link>
-                <Link to="/orders">Order History</Link>
-                <button onClick={() => setLoggedInUser({})}>Sign Out</button>
-            </nav>
-        </div>
+
+        <Container>
+            <Navbar bg="light" variant="light">
+                <Navbar.Brand href="#home"> <img id="logo" src={logo} alt="" /></Navbar.Brand>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                </Form>
+                <Nav className="ml-auto">
+                    <Nav.Link href="#home">News</Nav.Link>
+                    <Nav.Link href="#features">Destination</Nav.Link>
+                    <Nav.Link href="#pricing">Blog</Nav.Link>
+                    <Nav.Link href="#pricing">Contact</Nav.Link>
+                    <Button>Login</Button>
+                </Nav>
+
+            </Navbar>
+        </Container>
+
     );
 };
 
